75d26133df90daeffda211fe9d0d1d47ba6a82b8
yz3052 <yz3052@columbia.edu>
2026-03-03 16:37:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,137 +1,73 @@
+import requests
+
 import pandas as pd
 import numpy as np
-
-import json
-import os
-import time
 import datetime
+import time
+import os
 
-from man.delphi import ScrapingSession
+from ahl.web.proxy import proxy
 
-import hashlib
-import hmac
+import sys
+sys.path.append('/users/is/tozhang/PycharmProjects/projects/yz')
+import yz.util as yz
+
+import io
+
+import tqdm
 
 
-
-
-def md5_hex(string: str):
-    md5_str = hashlib.md5(string.encode('utf-8')).hexdigest()
-    return md5_str
-
-def hmac_md5(content, key_text):
-    try:
-        key_bytes = key_text.encode('utf-8')
-        content_bytes = content.encode('utf-8') if isinstance(content, str) else content
-        mac = hmac.new(key_bytes, content_bytes, hashlib.md5)
-        return mac.digest()
-    except Exception as e:
-        print(f"Error occurred while calculating HMAC-MD5: {e}")
-        return b''
-
-def hmac_md5_hex(content, key_text):
-    return hmac_md5(content, key_text).hex()
-
-def signature(appid, secret_key, params):
-    params['appid'] = appid
-    sorted_dict = dict(sorted(params.items(), key=lambda x: x[0]))
-    query_list = [f"{k}={v}" for k, v in sorted_dict.items()]
-    uri_query = '&'.join(query_list)
-    sign = hmac_md5_hex(uri_query, secret_key)
-    return sign
-
-def prepare_body(
-    beginTime: int, endTime: int,  pageStart: int
-):
-    return {"beginTime": beginTime, "endTime": endTime, "pageSize": 500, "pageStart": pageStart}
-
-def prepare_header(
-    body: dict
-):
-    appid = "b9df795f-eee2-4c36-8c0a-d22948926c25"
-    secret_key = "e16ecfe6df26403581f4509f6258a44c"
-    header = {
-        "body": md5_hex(json.dumps(body)),
-        "act": "ahl-list", # market
-        "app": "open-brm",
-        "mod": "roadshow",
-        "timestamp": str(int(time.time() * 1000))
-    }
-    res_sign = signature(appid, secret_key, header)
-    header["signature"] = res_sign
-    header["appid"] = appid
-
-    return header
+# this is scrape all CFFEX eod quotes
 
 
 
+########################################################################################################
+#!- constants
+########################################################################################################
+
+NOW = datetime.datetime.utcnow()
+TODAY_str = NOW.strftime('%Y%m%d')
+root_o = '/data/user/tozhang/mktdata/cffex/eod_mktdata/'
 
 
-if __name__ == '__main__':
 
-    # to be scheduled once every 30 minutes on each calendar day between 5am to 0am  (Asia/Shanghai timezone)
-
-
-    # get timestamps
-
-    NOWUTC = pd.to_datetime(datetime.datetime.utcnow())
-    NOW = NOWUTC.tz_localize('UTC').tz_convert('Asia/Shanghai').tz_localize(None)
-    TODAY = NOW.normalize()
-    TODAY_m1w = TODAY - pd.to_timedelta('7 days')
-    TODAY_p2m = TODAY + pd.to_timedelta('63 days')
+########################################################################################################
+#!- months to query
+########################################################################################################
 
 
-    # scrape
-
-    body = prepare_body(
-        beginTime = int(datetime.datetime.timestamp(TODAY_m1w)*1000),
-        endTime = int(datetime.datetime.timestamp(TODAY_p2m)*1000),
-        pageStart = 1
-    )
-
-    NEXT = True
-    o_data = []
-    while NEXT:
-        print(body['pageStart'], end= ',')
-
-        # API query
-
-        header = prepare_header(body)
-        session = ScrapingSession()
-        response = session.post(
-            url='https://server.comein.cn/comein/index.php'
-            , headers=header
-            , json=body
-        )
-        i_json = response.json()
+yyyymm_existing = [f[:6] for f in os.listdir(root_o)]
+yyyymm_2_query = list(set(pd.date_range('2019-01-01', TODAY_str).strftime('%Y%m')))
+yyyymm_2_query = [i for i in yyyymm_2_query if i not in yyyymm_existing]
 
 
-        # if not success
 
-        if i_json['msg'] != '成功':
-            raise Exception(i_json['errordesc'])
+with proxy():
 
-
-        # get data
-
-        t_data = pd.DataFrame(i_json['data'])
-        o_data.append(t_data)
+    for yyyymm in tqdm.tqdm(yyyymm_2_query):
 
 
-        # next for loop
+        url = f"http://www.cffex.com.cn/sj/historysj/{yyyymm}/zip/{yyyymm}.zip"
+        response = requests.get(url)
 
-        if i_json['extra']['hasMore']:
-            body['pageStart'] = body['pageStart'] + 1
-        else:
-            NEXT = False
+        # Load zip file into memory
+        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
 
+        # Get list of all CSV files
+        csv_files = [f for f in zip_file.namelist() if f.endswith('.csv')]
 
-    # output
+        # Read each CSV file one by one
+        for csv_file in csv_files:
+            with zip_file.open(csv_file) as csv_f:
+                t_data = pd.read_csv(csv_f, encoding='gbk')
+                t_data = t_data.rename(columns={
+                    '合约代码': 'code',
+                    '今开盘': 'o', '最高价': 'h', '最低价': 'l', '成交量': 'v', '成交金额': 'amt',
+                    '持仓量': 'oi', '持仓变化': 'oi_delta', '今收盘': 'c', '今结算': 'settle_px',
+                    '前结算': 'prev_settle_px',
+                    '涨跌1': 'chg1', '涨跌2': 'chg2', '隐含波动率(%)': 'iv_pvt',
+                })
+                t_data['t1d'] = pd.to_datetime(csv_file[:8], format='%Y%m%d')
+                t_data = t_data.dropna(subset=['o', 'c'])
+                t_data.to_parquet(os.path.join(root_o, csv_file[:8] + '.parquet'))
 
-    o_data_s2 = pd.concat(o_data, axis = 0)
-    o_data_s2['scraper_ts_cn'] = NOW
-    o_data_s2['scraper_ts_utc'] = NOWUTC
-    o_data_s2 = o_data_s2.sort_values('stime')
-
-    if not os.path.exists('/data/user/tozhang/comein/'+ NOW.strftime('%Y%m%d')):
-        os.mkdir('/data/user/tozhang/comein/'+ NOW.strftime('%Y%m%d'))
-    o_data_s2.to_parquet('/data/user/tozhang/comein/'+ NOW.strftime('%Y%m%d/%H%M%S.parquet'))
